b371a1280462811ddacf0e249eb5cdc3052cc53a
daodu <daodu@DESKTOP-NOSLD4D>
2023-02-10 20:02:20 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Tool_2M
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.login = new System.Windows.Forms.Button();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.STT = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.uid = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pass = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.haifa = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.token = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.status = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(101, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 27);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(277, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // login
+            // 
+            this.login.Location = new System.Drawing.Point(59, 85);
+            this.login.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(86, 31);
+            this.login.TabIndex = 4;
+            this.login.Text = "Login";
+            this.login.UseVisualStyleBackColor = true;
+            this.login.Click += new System.EventHandler(this.login_Click);
+            // 
+            // dgv
+            // 
+            this.dgv.AllowUserToAddRows = false;
+            this.dgv.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.STT,
+            this.uid,
+            this.pass,
+            this.haifa,
+            this.token,
+            this.status});
+            this.dgv.Location = new System.Drawing.Point(0, 249);
+            this.dgv.Name = "dgv";
+            this.dgv.RowHeadersWidth = 51;
+            this.dgv.RowTemplate.Height = 29;
+            this.dgv.Size = new System.Drawing.Size(985, 265);
+            this.dgv.TabIndex = 5;
+            //this.dgv.KeyUp += new System.Windows.Forms.KeyEventHandler(this.dgv_KeyUp);
+            // 
+            // STT
+            // 
+            this.STT.HeaderText = "STT";
+            this.STT.MinimumWidth = 6;
+            this.STT.Name = "STT";
+            // 
+            // uid
+            // 
+            this.uid.HeaderText = "UID";
+            this.uid.MinimumWidth = 6;
+            this.uid.Name = "uid";
+            // 
+            // pass
+            // 
+            this.pass.HeaderText = "PASS";
+            this.pass.MinimumWidth = 6;
+            this.pass.Name = "pass";
+            // 
+            // haifa
+            // 
+            this.haifa.HeaderText = "2FA";
+            this.haifa.MinimumWidth = 6;
+            this.haifa.Name = "haifa";
+            // 
+            // token
+            // 
+            this.token.HeaderText = "Token";
+            this.token.MinimumWidth = 6;
+            this.token.Name = "token";
+            // 
+            // status
+            // 
+            this.status.HeaderText = "Trạng thái";
+            this.status.MinimumWidth = 6;
+            this.status.Name = "status";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(983, 516);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.login);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Button button1;
+        private Button login;
+        private DataGridView dgv;
+        private DataGridViewTextBoxColumn STT;
+        private DataGridViewTextBoxColumn uid;
+        private DataGridViewTextBoxColumn pass;
+        private DataGridViewTextBoxColumn haifa;
+        private DataGridViewTextBoxColumn token;
+        private DataGridViewTextBoxColumn status;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,115 @@
+﻿namespace Tool_2M
+{
+    using Newtonsoft.Json.Linq;
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
+    using OtpNet;
+    using System;
+    using System.Windows.Forms;
+    using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void login_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < dgv.Rows.Count; i++)
+            {
+                DataGridViewRow dataGridViewRow = dgv.Rows[i];
+                String uid = dataGridViewRow.Cells["uid"].Value.ToString();
+                String pass = dataGridViewRow.Cells["pass"].Value.ToString();
+                String haiFA = dataGridViewRow.Cells["haifa"].Value.ToString();
+
+                ChromeOptions chromeOptions = new ChromeOptions();
+                ChromeDriverService chromeDriverService = ChromeDriverService.CreateDefaultService();
+                chromeDriverService.HideCommandPromptWindow = true;
+
+                chromeOptions.AddArgument("--window-size=630,515");
+                chromeOptions.AddArgument("--disable-notifications");
+                chromeOptions.AddArgument("--disable-images");
+                chromeOptions.AddArgument("--mute-audio");
+
+                String profileFloder = "C:\\Users\\" + Environment.UserName + "\\AppData\\Local\\Google\\Chrome\\User Data";
+                String profileFloderUid = "C:\\Users\\" + Environment.UserName + "\\AppData\\Local\\Google\\Chrome\\User Data\\" + uid;
+                bool existsProfile = false;
+                if (Directory.Exists(profileFloderUid))
+                {
+                    existsProfile = true;
+                }
+                else
+                {
+                    Directory.CreateDirectory(profileFloderUid);
+                }
+                chromeOptions.AddArgument(@"user-data-dir=" + profileFloderUid);
+
+
+                ChromeDriver driver = new ChromeDriver(chromeDriverService, chromeOptions);
+                driver.Navigate().GoToUrl("https://mbasic.facebook.com");
+                Thread.Sleep(2000);
+
+                if (!existsProfile)
+                {
+                    driver.FindElement(By.XPath("/html/body/div/div/div[2]/div/table/tbody/tr/td/div[2]/div/div[2]/form/ul/li[1]/input")).SendKeys(uid);
+                    driver.FindElement(By.XPath("/html/body/div/div/div[2]/div/table/tbody/tr/td/div[2]/div/div[2]/form/ul/li[2]/section/input")).SendKeys(pass);
+                    Thread.Sleep(3000);
+                    driver.FindElement(By.XPath("/html/body/div/div/div[2]/div/table/tbody/tr/td/div[2]/div/div[2]/form/ul/li[3]/input")).Click();
+                    Thread.Sleep(3000);
+                    byte[] bytes = Base32Encoding.ToBytes(haiFA);
+                    Totp totp = new Totp(bytes);
+                    String code2Fa = totp.ComputeTotp();
+                    //MessageBox.Show(code2Fa);
+                    driver.FindElement(By.XPath("/html/body/div/div/div[2]/div/form/div[1]/article/section/section[2]/div[2]/div/input")).SendKeys(code2Fa); //nhap code 2fa
+
+                    Thread.Sleep(3000);
+                    driver.FindElement(By.XPath("/html/body/div/div/div[2]/div/form/div[1]/article/div[1]/table/tbody/tr/td/input")).Click(); // click tiep tuc 2fa
+
+                    Thread.Sleep(3000);
+                    driver.FindElement(By.XPath("/html/body/div/div/div[2]/div/form/div[1]/article/div[1]/table/tbody/tr/td/input")).Click();
+
+
+                }
+                goToBusinessLocation(driver, dataGridViewRow);
+
+            }
+
+        }
+    }
+
+    private async void goToBusinessLocation(ChromeDriver driver, DataGridViewRow dataGridViewRow)
+    {
+        driver.Navigate().GoToUrl("https://business.facebook.com/business_locations");
+        Thread.Sleep(3000);
+        if (driver.Url.Contains("https://business.facebook.com/security/twofactor/reauth"))
+        {
+            Thread.Sleep(2000);
+            HttpClient client = new HttpClient();
+            var json = await client.GetStringAsync("https://2fa.live/tok/" + dataGridViewRow.Cells["haifa"].Value.ToString());
+            JObject jsonObject = JObject.Parse(json);
+            string twoHaiFaValue = jsonObject["token"].ToString();
+            driver.FindElement(By.XPath("/html/body/div[1]/div[1]/div/div[2]/div/div/div/div/div/div[2]/div[1]/div[4]/span/span/div/div[2]/div/div/div/div[1]/div[2]/div/div/input")).SendKeys(twoHaiFaValue);
+            Thread.Sleep(3000);
+            driver.FindElement(By.XPath("/html/body/div[1]/div[1]/div/div[2]/div/div/div/div/div/div[2]/div[1]/div[4]/span/span/div/div[2]/div/div/div/div[1]/div[2]/div/div/input")).SendKeys(OpenQA.Selenium.Keys.Enter);
+            Thread.Sleep(3000);
+        }
+        String value = driver.PageSource;
+        int vitriEaag = value.IndexOf("EAAG");
+        value = value.Remove(0, vitriEaag);
+        string[] strings
+            = value.Split('\"');
+        string tokenEaag = strings[0];
+        //MessageBox.Show(strings[0]);
+        dataGridViewRow.Cells["token"].Value = tokenEaag;
+        dataGridViewRow.Cells["status"].Value = "Đã Lấy được token EAAG";
+    }
+
+}
